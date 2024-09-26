d11d93b5472d9ff244d845ddfdcaf339161ab6d9
kuma4188 <psb4188@gmail.com>
2024-09-26 20:25:23 +0900

    refactor : service에서 유저프로필 업데이트

--- src/main/java/challenging/application/service/UserProfileService.java
+++ src/main/java/challenging/application/service/UserProfileService.java
@@ -1,23 +1,18 @@
 package challenging.application.service;
 
+import challenging.application.dto.UserProfileResponseDTO;
 import challenging.application.model.UserProfile;
-import challenging.application.repository.UserProfileRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.Map;
+import java.util.Optional;
 
-@Service
-public class UserProfileService {
+public interface UserProfileService {
+    Optional<UserProfile> getUserProfileByEmail(String email);
 
-    @Autowired
-    private UserProfileRepository userProfileRepository;
+    UserProfile saveUserProfile(UserProfile userProfile);
 
-    // 이메일을 기반으로 UserProfile을 조회하는 메소드
-    public UserProfile getUserProfileByEmail(String email) {
-        return userProfileRepository.findByUserId(email).orElse(null);
-    }
+    UserProfileResponseDTO convertToDTO(UserProfile userProfile);
 
-    // 새로운 UserProfile 저장 메소드
-    public UserProfile saveUserProfile(UserProfile userProfile) {
-        return userProfileRepository.save(userProfile);
-    }
+    UserProfile createNewUserProfile(String email);
+
+    boolean updateUserProfileFields(UserProfile userProfile, Map<String, String> updates);
 }
