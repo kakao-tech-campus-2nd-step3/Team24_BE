8c5f3d0d892e66fa269b4e45df0ee2fffd11f2de
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-09-30 19:54:50 +0900

    feat : 이미지 처리 예시 컨트롤러 구현

--- src/main/java/challenging/application/s3/ImageController.java
+++ src/main/java/challenging/application/s3/ImageController.java
@@ -1,5 +1,20 @@
 package challenging.application.s3;
 
+import java.util.Map;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartRequest;
+
+@RestController
 public class ImageController {
 
+    @PostMapping("/image/upload")
+    @ResponseBody
+    public Map<String, Object> imageUpload(MultipartRequest multipartRequest){
+
+
+
+        return 
+    }
 }
