b34c17f3b30b6446d15872c143b7012d1ed88906
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-09-30 20:34:35 +0900

    feat : 예시컨트롤러 추가

--- src/main/java/challenging/application/images/ImageController.java
+++ src/main/java/challenging/application/images/ImageController.java
@@ -1,5 +1,7 @@
 package challenging.application.images;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,12 +11,27 @@ import org.springframework.web.multipart.MultipartRequest;
 @RestController
 public class ImageController {
 
+    private final ImageService imageService;
+
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
     @PostMapping("/image/upload")
     @ResponseBody
-    public Map<String, Object> imageUpload(MultipartRequest multipartRequest){
+    public Map<String, Object> imageUpload(MultipartRequest multipartRequest)  {
+
+        Map<String, Object> responseData = new HashMap<>();
+
+        try{
+            String s3Url = imageService.imageload(multipartRequest);
 
 
-
-        return Res
+            responseData.put("uploaded",true);
+            responseData.put("url",s3Url);
+        } catch (IOException e){
+            responseData.put("uploaded",false);
+        }
+        return responseData;
     }
 }
