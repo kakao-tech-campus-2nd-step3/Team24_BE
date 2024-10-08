729767595809feab649ff9fb062a726057c6894d
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-10-08 23:01:17 +0900

    feat : get url 발급 로직 구현

--- src/main/java/challenging/application/images/S3Uploader.java
+++ src/main/java/challenging/application/images/S3Uploader.java
@@ -83,6 +83,48 @@ public class S3Uploader {
         }
     }
 
+    public String createPresignedGetUrl(String imageExtension, String uuid) {
+        String keyName = "images/" + uuid + "." + imageExtension;
+        keyName = keyName.replace("-", "");
+
+        try {
+            // presigned URL 생성 (GET 요청)
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucket)
+                .key(keyName)
+                .build();
+
+            GetObjectPresignRequest getPresignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(DURATION_URL_TIME))  // URL expires in 10 minutes.
+                .getObjectRequest(getObjectRequest)
+                .build();
+
+            PresignedGetObjectRequest presignedGetObjectRequest = s3Presigner.presignGetObject(getPresignRequest);
+            String downloadUrl = presignedGetObjectRequest.url().toString();
+
+            log.info("Presigned URL to download a file from: {}", downloadUrl);
+
+            return downloadUrl;
+
+        } catch (AmazonS3Exception e) {
+            // S3 서비스에서 발생하는 오류를 처리
+            log.error("S3 관련 오류 발생. 상태 코드: {}, 오류 메시지: {}", e.getStatusCode(), e.getErrorMessage());
+            throw new RuntimeException("S3에서 presigned URL을 생성하는 중 오류가 발생했습니다.", e);
+        } catch (AmazonServiceException e) {
+            // AWS 서비스 전체에서 발생하는 오류를 처리
+            log.error("AWS 서비스에서 오류 발생. 상태 코드: {}, AWS 오류 코드: {}, 요청 ID: {}",
+                e.getStatusCode(), e.getErrorCode(), e.getRequestId());
+            throw new RuntimeException("AWS 서비스에서 presigned URL을 생성하는 중 오류가 발생했습니다.", e);
+        } catch (SdkClientException e) {
+            // SDK 클라이언트 측에서 발생하는 오류를 처리 (예: 네트워크 문제)
+            log.error("클라이언트 측 오류 발생: {}", e.getMessage());
+            throw new RuntimeException("AWS SDK 클라이언트에서 presigned URL을 생성하는 중 오류가 발생했습니다.", e);
+        } catch (Exception e) {
+            // 기타 모든 예외 처리
+            log.error("예상치 못한 오류 발생: {}", e.getMessage());
+            throw new RuntimeException("Presigned URL 생성 중 알 수 없는 오류가 발생했습니다.", e);
+        }
+    }
 
 
     private void validateImageExtension(String extension) {
