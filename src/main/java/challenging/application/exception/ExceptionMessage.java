6970db8045e2af9799eeb75fa8d6f77353407a4e
JJY <ddglackrp@gmail.com>
2024-10-08 19:46:45 +0900

    feat "Exception message 추가"

--- src/main/java/challenging/application/exception/ExceptionMessage.java
+++ src/main/java/challenging/application/exception/ExceptionMessage.java
@@ -7,6 +7,5 @@ public class ExceptionMessage {
     public static final String UNSUPPORTED_JWT_EXCEPTION = "지원하지 않는 Token 입니다.";
     public static final String SIGNATURE_EXCEPTION = "Token 의 서명이 유효하지 않습니다.";
     public static final String JWT_EXCEPTION = "잘못된 Token 입니다.";
-
-
+    public static final String HISTORY_NOT_FOUND = "존재 하지 않는 히스토리 입니다.";
 }
