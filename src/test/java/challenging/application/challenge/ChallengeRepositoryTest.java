66dcd4ce866905ce3917f76dec4d1721bb34f727
Gahyeon <best11gh@naver.com>
2024-10-10 22:24:54 +0900

    test: challenge repository test 코드 수정

--- src/test/java/challenging/application/challenge/ChallengeRepositoryTest.java
+++ src/test/java/challenging/application/challenge/ChallengeRepositoryTest.java
@@ -1,20 +1,25 @@
 package challenging.application.challenge;
 
+import challenging.application.auth.domain.Member;
+import challenging.application.auth.repository.MemberRepository;
+import challenging.application.challenge.domain.Category;
 import challenging.application.challenge.domain.Challenge;
 import challenging.application.challenge.repository.ChallengeRepository;
-import challenging.application.domain.Category;
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.*;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
 @Transactional
@@ -23,23 +28,33 @@ class ChallengeRepositoryTest {
   @Autowired
   private ChallengeRepository challengeRepository;
 
+  @Autowired
+  private MemberRepository memberRepository;
+
   @BeforeEach
   void setUp() {
+    Member host = new Member("테스트하는 사람", "테스트 닉네임", "test@example.com", "ROLE_USER");
+    memberRepository.save(host);
+    memberRepository.save(host);
+
     Challenge challenge1 = Challenge.builder()
-        .category(Category.SPORTS)
+        .host(host)
+        .category(Category.SPORT)
         .name("아침 조깅")
         .body("매일 아침 조깅 챌린지에 참여하세요.")
         .point(100)
         .date(LocalDate.of(2024, 11, 11))
         .startTime(LocalTime.of(6, 0))
         .endTime(LocalTime.of(7, 0))
-        .imageUrl("https://i.namu.wiki/i/h1OsaRJTYVMZqNJgV18rsMgnuW93lXxkWb1ujRDctn0egswSk1VXIEeZVBZ9zRad-PK9S1YhMTyFWTKK2lyEJieoSiOHBERaR4ilxP-7zjbsZvVC-muvIA50Of-aCAnJBj6NGrRv7j4uwXQfCDlneA.webp")
+        .imageUrl(
+            "https://i.namu.wiki/i/h1OsaRJTYVMZqNJgV18rsMgnuW93lXxkWb1ujRDctn0egswSk1VXIEeZVBZ9zRad-PK9S1YhMTyFWTKK2lyEJieoSiOHBERaR4ilxP-7zjbsZvVC-muvIA50Of-aCAnJBj6NGrRv7j4uwXQfCDlneA.webp")
         .minParticipantNum(5)
         .maxParticipantNum(20)
         .build();
 
     Challenge challenge2 = Challenge.builder()
-        .category(Category.SPORTS)
+        .host(host)
+        .category(Category.SPORT)
         .name("저녁 조깅")
         .body("매일 저녁 조깅 챌린지에 참여하세요.")
         .point(100)
@@ -55,28 +70,27 @@ class ChallengeRepositoryTest {
     challengeRepository.save(challenge2);
   }
 
-  @Test
-  @DisplayName("카테고리와 날짜로 챌린지를 조회 - 정상적으로 조회")
-  void findByCategoryAndDateTest() {
-    List<Challenge> foundChallenges = challengeRepository.findByCategoryAndDate(Category.SPORTS, LocalDate.of(2024, 11, 11));
-
-    assertThat(foundChallenges).isNotEmpty();
-    assertThat(foundChallenges.size()).isEqualTo(2);
+  static Stream<Arguments> dateProvider() {
+    return Stream.of(
+        Arguments.of(LocalDateTime.of(2024, 11, 10, 5, 0), 2), // 전날 -> 2개
+        Arguments.of(LocalDateTime.of(2024, 11, 11, 5, 0), 2), // 5시 -> 2개
+        Arguments.of(LocalDateTime.of(2024, 11, 11, 6, 0), 1), // 6시 -> 1개
+        Arguments.of(LocalDateTime.of(2024, 11, 11, 19, 0), 0),  // 19시 -> 0개
+        Arguments.of(LocalDateTime.of(2024, 11, 12, 5, 0), 0) // 다음날 -> 0개
+    );
   }
 
-  @Test
-  @DisplayName("잘못된 카테고리로 조회 - 빈 리스트 반환")
-  void findByInvalidCategoryTest() {
-    List<Challenge> foundChallenges = challengeRepository.findByCategoryAndDate(Category.HOBBY, LocalDate.of(2024, 11, 11));
+  @ParameterizedTest
+  @MethodSource("dateProvider")
+  @DisplayName("카테고리와 날짜로 챌린지 검색")
+  void testFindByCategoryAndDateAndStartTimeAfter(LocalDateTime queryDate, int expectedCount) {
+    List<Challenge> challenges = challengeRepository.findByCategoryAndDateTimeAfter(
+        Category.SPORT,
+        queryDate.toLocalDate(),
+        queryDate.toLocalTime()
+    );
 
-    assertThat(foundChallenges).isEmpty();
+    assertEquals(expectedCount, challenges.size());
   }
 
-  @Test
-  @DisplayName("잘못된 날짜로 조회 - 빈 리스트 반환")
-  void findByInvalidDateTest() {
-    List<Challenge> foundChallenges = challengeRepository.findByCategoryAndDate(Category.SPORTS, LocalDate.of(2025, 1, 1));
-
-    assertThat(foundChallenges).isEmpty();
-  }
 }
