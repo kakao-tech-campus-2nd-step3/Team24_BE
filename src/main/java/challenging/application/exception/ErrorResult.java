8a059cfface312334482e4a5be6c05e68c7ec616
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-10-30 21:10:50 +0900

    feat : "Response 관련 수정"

--- src/main/java/challenging/application/exception/ErrorResult.java
+++ src/main/java/challenging/application/exception/ErrorResult.java
@@ -10,14 +10,8 @@ public class ErrorResult {
     private final String code;
     private final String message;
 
-    private final Map<String, String> validation = new HashMap<>();
-
     public ErrorResult(String code, String message) {
         this.code = code;
         this.message = message;
-    }
-
-    public void addValidation(String fieldName, String errorMessage) {
-        this.validation.put(fieldName, errorMessage);
     }
 }
---
+++ src/main/java/challenging/application/exception/ErrorValidationResult.java
@@ -0,0 +1,21 @@
+package challenging.application.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.Getter;
+
+@Getter
+public class ErrorValidationResult {
+    private final String code;
+    private final String message;
+    private final Map<String, String> validation = new HashMap<>();
+
+    public ErrorValidationResult(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public void addValidation(String fieldName, String errorMessage) {
+        this.validation.put(fieldName, errorMessage);
+    }
+}
