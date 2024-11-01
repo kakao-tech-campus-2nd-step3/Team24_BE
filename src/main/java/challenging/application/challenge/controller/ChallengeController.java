ddb13e18296b2e8b2c212ca871a2d074b9e43d44
Gahyeon <best11gh@naver.com>
2024-11-01 15:26:11 +0900

    refactor: 챌린지 - response util 적용

--- src/main/java/challenging/application/challenge/controller/ChallengeController.java
+++ src/main/java/challenging/application/challenge/controller/ChallengeController.java
@@ -20,65 +20,69 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/challenges")
 public class ChallengeController {
 
-  private final ChallengeService challengeService;
+    private final ChallengeService challengeService;
 
-  public ChallengeController(ChallengeService challengeService) {
-    this.challengeService = challengeService;
-  }
+    public ChallengeController(ChallengeService challengeService) {
+        this.challengeService = challengeService;
+    }
 
-  // 챌린지 단건 조회
-  @GetMapping("/{challengeId}")
-  public ResponseEntity<ApiResponse<ChallengeResponse>> getChallenge(
-      @PathVariable Long challengeId) {
+    // 챌린지 단건 조회
+    @GetMapping("/{challengeId}")
+    public ResponseEntity<ApiResponse<ChallengeResponse>> getChallenge(
+            @PathVariable Long challengeId) {
 
-    ChallengeResponse response = challengeService.getChallengeById(challengeId);
+        ChallengeResponse response = challengeService.getChallengeById(challengeId);
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(201,response));
-  }
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>("success", 200, "챌린지 조회가 완료되었습니다.", response));
+    }
 
-  // 챌린지 카테고리 조회
-  @GetMapping("/category/{categoryId}")
-  public ResponseEntity<ApiResponse<List<ChallengeResponse>>> getChallengesByCategory(
-      @PathVariable int categoryId,
-      @RequestBody DateRequest dateRequest) {
+    // 챌린지 카테고리 조회
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<ApiResponse<List<ChallengeResponse>>> getChallengesByCategory(
+            @PathVariable int categoryId,
+            @RequestBody DateRequest dateRequest) {
 
-    List<ChallengeResponse> responses = challengeService.getChallengesByCategoryAndDate(
-        categoryId, dateRequest.date());
+        List<ChallengeResponse> responses = challengeService.getChallengesByCategoryAndDate(
+                categoryId, dateRequest.date());
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(201,responses));
-  }
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>("success", 200, "카테고리별 챌린지 조회가 완료되었습니다.", responses));
+    }
 
-  // 챌린지 생성
-  @PostMapping
-  public ResponseEntity<ApiResponse<ChallengeCreateResponse>> createChallenge(
-      @RequestBody ChallengeRequest challengeRequestDTO) {
+    // 챌린지 생성
+    @PostMapping
+    public ResponseEntity<ApiResponse<ChallengeCreateResponse>> createChallenge(
+            @RequestBody ChallengeRequest challengeRequestDTO) {
 
-    ChallengeCreateResponse response = challengeService.createChallenge(challengeRequestDTO);
+        ChallengeCreateResponse response = challengeService.createChallenge(challengeRequestDTO);
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(201,response));
-  }
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ApiResponse<>("success", 201, "챌린지 생성이 완료되었습니다.", response));
+    }
 
-  // 챌린지 삭제
-  @DeleteMapping("{challengeId}")
-  public ResponseEntity<ApiResponse<ChallengeDeleteResponse>> deleteChallenge(
-      @PathVariable Long challengeId,
-      @LoginMember Member loginMember
-  ) {
-    ChallengeDeleteResponse response = challengeService.deleteChallenge(challengeId, loginMember);
+    // 챌린지 삭제
+    @DeleteMapping("{challengeId}")
+    public ResponseEntity<ApiResponse<ChallengeDeleteResponse>> deleteChallenge(
+            @PathVariable Long challengeId,
+            @LoginMember Member loginMember
+    ) {
+        ChallengeDeleteResponse response = challengeService.deleteChallenge(challengeId, loginMember);
 
-    return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(200,response));
-  }
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>("success", 200, "챌린지 삭제가 완료되었습니다.", response));
+    }
 
-  // 챌린지 예약
-  @PostMapping("/reservation/{challengeId}")
-  public ResponseEntity<?> reserveChallenge(
-      @PathVariable Long challengeId,
-      @LoginMember Member loginMember
-  ) {
-    ChallengeReservationResponse challengeResponse = challengeService.reserveChallenge(challengeId, loginMember);
+    // 챌린지 예약
+    @PostMapping("/reservation/{challengeId}")
+    public ResponseEntity<ApiResponse<?>> reserveChallenge(
+            @PathVariable Long challengeId,
+            @LoginMember Member loginMember
+    ) {
+        ChallengeReservationResponse challengeResponse = challengeService.reserveChallenge(challengeId, loginMember);
 
-    return ResponseEntity.status(HttpStatus.OK)
-        .contentType(MediaType.APPLICATION_JSON)
-        .body(challengeResponse);
-  }
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ApiResponse<>("success", 200, "챌린지 예약이 완료되었습니다.", challengeResponse));
+    }
 }
