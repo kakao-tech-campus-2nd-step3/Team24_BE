753d42dbff4d306bfc25902dd2cfaeb2c8f10d58
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-11-13 19:32:45 +0900

    feat : "vote 로직 완성"

--- src/main/java/challenging/application/domain/participant/repository/ParticipantRepository.java
+++ src/main/java/challenging/application/domain/participant/repository/ParticipantRepository.java
@@ -1,10 +1,12 @@
 package challenging.application.domain.participant.repository;
 
 import challenging.application.domain.participant.entity.Participant;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
   Integer countByChallengeId(Long challengeId);
   boolean existsByChallengeIdAndMemberId(Long challengeId, Long memberId);
+  List<Participant> findAllByChallengeId(Long challengeId);
 
 }
